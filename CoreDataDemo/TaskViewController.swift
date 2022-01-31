1c8bbdc9c725273a5fe1605b3acb4199354ae017
ilyabill <bilarusilya@gmail.com>
2022-01-31 11:04:28 +0300

    Prepare Task ViewController

--- CoreDataDemo/TaskViewController.swift
+++ CoreDataDemo/TaskViewController.swift
@@ -8,22 +8,81 @@
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    
+    private lazy var taskTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "new Task"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        createButton(
+            withTitle: "Save Task",
+            andColor: UIColor(named: "Save") ?? UIColor.green,
+            action: UIAction(handler: { _ in
+            self.dismiss(animated: true)
+        }))
+    }()
+    
+    
+    private lazy var cancelButton: UIButton = {
+        createButton(
+            withTitle: "Cancel",
+            andColor: UIColor(named: "Cancel") ?? UIColor.red,
+            action: UIAction(handler: { _ in
+            self.dismiss(animated: true)
+        }))
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        setup(subviews: taskTextField, saveButton, cancelButton)
+        setConstraints()
+    }
+
+    
+    private func setup(subviews: UIView...) {
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setConstraints() {
+        taskTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            taskTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor, constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor, constant: 20),
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
     }
-    */
-
+    
+    private func createButton(withTitle title: String, andColor color: UIColor, action: UIAction) -> UIButton {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = color
+        
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        buttonConfiguration.attributedTitle = AttributedString(title, attributes: attributes)
+        
+        return UIButton(configuration: buttonConfiguration, primaryAction: action)
+    }
+    
 }
